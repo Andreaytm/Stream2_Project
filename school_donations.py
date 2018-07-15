706ca57078009c76c0c9aa068cc98820846bc62e
Andreaytm <andrea.ma@hotmail.co.uk>
2018-07-15 14:59:12 +0100

    Set up database connection and connection call

---
+++ school_donations.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
+
+app = Flask(__name__)
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'schooldonors'
+COLLECTION_NAME = 'donations'
+
+
+@app.route("/")
+def index():
+    """
+    Flask view for main dashboard page
+    """
+    return render_template("index.html")
+
+
+@app.route("/schooldonors/donations")
+def schooldonors():
+    """
+    Flask view to serve project data from MongoDB in JSON format
+    """
+
+    # Constant to define the record fields that are to be retrieved
+    FIELDS = {
+        'id': False, 'funding_status': True, 'school_state': True,
+        'resource_type': True, 'poverty_level': True,
+        'date_posted': True, 'total_donations': True,
+        'primary_focus_area': True, 'primary_focus_subject': True,
+        'teacher_teach_for_america': True, 'grade_level': True
+    }
+
+    # Open a connection to MongoDB using a 'with' statement such that the
+    # connection will close as soon as exit the 'with' statement
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+        # Define wish to access collection: donations
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        # Retrieve result set only with fields defined in FIELDS, given count no need to limit results
+        donations = collection.find(projection=FIELDS)
+        # Convert donations to a list in a JSON object and return JSON data
+        return json.dumps(list(donations))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
