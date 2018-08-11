3008ea5bad1cfb7024ab4b8d9fe7e63c68bd8cbb
Andreaytm <andrea.ma@hotmail.co.uk>
2018-08-11 17:46:40 +0100

    Added scroll button to pages

---
+++ static/js/scrollbutton.js
@@ -0,0 +1,18 @@
+/* adapted from www.w3schools.com */
+window.onscroll = function() {
+	scroll();
+};
+
+function scroll() {
+	if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+		document.getElementById("toTop").style.display = "block";
+	} else {
+		document.getElementById("toTop").style.display = "none";
+	}
+}
+
+// When the user clicks on button scroll to the top of the page
+function GoToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
